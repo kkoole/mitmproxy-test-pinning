5866ed126122a71edcedca408e5ad216ca61a1b7
kkoole <kkoole@os3.nl>
2023-05-22 14:46:23 +0200

    Add debug

--- client_hello_addon.py
+++ client_hello_addon.py
@@ -2,7 +2,18 @@ from mitmproxy import ctx
 
 
 def request(flow):
-    if flow.client_conn and flow.client_conn.tls_established:
+    debug = True
+    if debug:
+        # get certificate list from server connection
+        certs = flow.server_conn.certificate_list
+        # loop through certificate list and get some attrs
+        for n, cert in enumerate(certs):
+            ctx.log('DEBUG: server "{}" cert "{}"'.format(n, cert))
+            ctx.log('DEBUG: server "{}" cert issuer "{}"'.format(n, cert.issuer))
+            ctx.log('DEBUG: server "{}" cert subject "{}"'.format(n, cert.subject))
+
+    # other things to be extracted
+    if not debug and flow.client_conn and flow.client_conn.tls_established:
         tls_client_certificate_list = flow.client_conn.certificate_list
         if tls_client_certificate_list:
             ctx.log('Certificate list "{}"'.format(tls_client_certificate_list))
